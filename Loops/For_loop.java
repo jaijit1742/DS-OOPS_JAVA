67e571839696692facec73dff3ae5c9336e3c3ee
Jaijit Singh <singhjaijit22@gmail.com>
2025-09-11 18:22:19 +0530

    Print numbers using for loop

---
+++ Loops/For_loop.java
@@ -0,0 +1,11 @@
+public class For_loop{
+    public static void main(String[] args) {
+        for(int i=0;i<=10;i++){
+            System.out.print(i);
+            if(i<10){
+                System.out.print(",");
+                System.out.print(" ");
+            }
+        }
+    }
+}
