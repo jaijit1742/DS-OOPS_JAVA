a4da38d1a9a879e27478712b4bed667c307aee9f
Jaijit Singh <singhjaijit22@gmail.com>
2025-09-18 10:42:45 +0530

    Fibonacci series using iteration

---
+++ Recursion/Series.java
@@ -0,0 +1,17 @@
+package Recursion;
+import java.util.Scanner;
+public class Series {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+            int a = 0, b = 1;
+        for (int i = 0; i < num; i++) {
+            int c = a + b;
+            System.out.println(c);
+            a = b;
+            b = c;
+
+        }
+        input.close();
+    }
+}
