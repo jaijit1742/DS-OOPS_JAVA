ace0cd58156b967e1a179c1533f869b5eeb813a2
Jaijit Singh <singhjaijit22@gmail.com>
2025-09-22 10:00:01 +0530

    Sum of elements of Array in O(n) Complexity

---
+++ Complexity_Analysis/Sum.java
@@ -0,0 +1,19 @@
+package Complexity_Analysis;
+
+import java.util.Scanner;
+public class Sum {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        int n= input.nextInt();
+        int[] arr= new int[n];
+        for(int i=0;i<n;i++){
+            arr[i]=input.nextInt();
+        }
+        int sum=0;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+        }
+        System.out.println(sum);
+        //Time complexity: O(n)
+    }
+}
